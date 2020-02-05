91351f8d5b21ed6922cb7a76782bd59734433448
antimodes201 <bob.krause201@gmail.com>
2020-02-04 22:33:38 -0500

    updated base image

--- start.sh
+++ start.sh
@@ -2,22 +2,30 @@
 # Startup Script for Citadel Forged with Fire Dedicated Server
 # Used in conjuncution with Docker Image for service
 
-dirPath=/citadel/Citadel/Saved/Config/LinuxServer
+if [ ${BRANCH} == public ]
+then
+        # GA Branch
+        /steamcmd/steamcmd.sh +login anonymous +force_install_dir /app +app_update 489650 +quit
+else
+        # used specified branch
+        /steamcmd/steamcmd.sh +login anonymous +force_install_dir /app +app_update 489650 -beta ${BRANCH} +quit
+fi
+
+dirPath=/app/Citadel/Saved/Config/LinuxServer
 gameini=${dirPath}/Game.ini
 engineini=${dirPath}/Engine.ini
+checkBoot=/app/checkBoot
 
-if [ ! -f "${gameini}" ]
+if [ ! -f "${checkBoot}" ]
 then
+	rm ${gameini}
+	rm ${engineini}
 	cp /scripts/Game.ini ${gameini}
-fi
-
-if [ ! -f "${engineini}" ]
-then
 	cp /scripts/Engine.ini ${engineini}
+	printf "Config Written In\n" > ${checkBoot}
 fi
 
-# Check for updates since build
-/steamcmd/steamcmd.sh +login anonymous +force_install_dir /citadel +app_update 489650 +quit
+cp /steamcmd/linux64/steamclient.so /app/Citadel/Plugins/UWorks/Source/ThirdParty/Linux/steamclient.so
 
 # Start Server
-/citadel/CitadelServer.sh
+/app/CitadelServer.sh
